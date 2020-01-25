3179f93884c746df4b3032be531f4896df3c5a4e
OliviaL18 <oliviatloos@gmail.com>
2020-01-25 07:51:51 -0500

    started variables for questions + answers

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    var questions = [
+        "What is the correct syntax for referring to an external script?", //1
+        "", //2
+        "", //3
+        "", //4
+        "", //5
+        "", //6
+    ];
+
+    var answerA = [
+        "<script='script.js'>", //1
+        "", //2
+        "", //3
+        "", //4
+        "", //5
+        "", //6
+    ];
+
+    var answerB = [
+        "<script src='script.js'>", //1
+        "", //2
+        "", //3
+        "", //4
+        "", //5
+        "", //6
+    ];
+
+    var answerC = [
+        "<script name='script.js'>", //1
+        "", //2
+        "", //3
+        "", //4
+        "", //5
+        "", //6
+    ];
+
+    var answerD = [
+        "script src='script.js'", //1
+        "", //2
+        "", //3
+        "", //4
+        "", //5
+        "", //6
+    ];
+}
